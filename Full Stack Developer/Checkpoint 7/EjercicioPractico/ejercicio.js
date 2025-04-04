b0607e1fd9ab159ca07bc88fa683afb274b63e0d
jdominguez1065 <137999461+jdominguez1065@users.noreply.github.com>
2025-04-04 20:41:13 +0200

    Add files via upload

---
+++ Full Stack Developer/Checkpoint 7/EjercicioPractico/ejercicio.js
@@ -0,0 +1,10 @@
+function miFuncion(num1, num2, num3, num4) {
+    let result = (num1 + num2) * (num3 + num4)
+    if (result < 50) {
+        console.log('¡El número es menor que 50!');
+    } else {
+        console.log('¡El número es mayor que 50!');
+    }
+}
+
+miFuncion(1, 2, 3, 4) 
